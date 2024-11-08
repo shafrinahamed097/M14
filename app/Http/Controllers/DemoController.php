41a64f66e79ff65459fd7eea219706a6ed9a48ac
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-08 16:52:09 +0600

    Request - working with Request Header

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    function DemoAction(Request $request):array{
-        return $request->input();
+    function DemoAction(Request $request):string{
+     $firstName = $request->header("firstName");
+     $lastName = $request->header("lastName");
+     $age = $request->header("age");
+
+     return "My Name is $firstName $lastName and I am $age years old";
     }
 }
