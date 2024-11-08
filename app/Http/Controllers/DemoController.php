4e5a87cfce1bf6051f74473438a1aebdf3e37830
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-08 12:04:05 +0600

    Working With request parameter

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    function DemoAction():string{
-        return "This is my first request response";
+    function DemoAction(Request $request):string{
+        $name=$request->name;
+        $age=$request->age;
+        return "My Name is {$name}, I'm {$age} years old.";
     }
 }
