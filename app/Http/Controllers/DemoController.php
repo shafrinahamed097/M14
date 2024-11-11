43dd1d98b195189329e6ddac278500bc1d776a60
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-11 11:34:17 +0600

    Response - Response Header & Response View

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -14,16 +14,7 @@ class DemoController extends Controller
     }
 
    function DemoAction(){
-    $name="token";
-    $value="123XYZ";
-    $minutes=120;
-    $path="/";
-    $domain=$_SERVER['SERVER_NAME'];
-    $secure=false;
-    
-    $httpOnly=true;
-
-    return response("Hi")->cookie($name,$value,$minutes,$path,$domain,$secure,$httpOnly);
+   return view('page.Home');
    }
  
 }
